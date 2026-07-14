3ceaeecc2eba3874a7769790e52efc4616db9282
NguyenNgocBuu <nguyenngocbuu03@gmail.com>
2026-07-14 11:19:28 +0700

    Update SmartTravelNguHanhSon from DuAn

---
+++ SmartTravelNguHanhSon/frontend/src/config/env.ts
@@ -0,0 +1,3 @@
+// Khi chạy trên điện thoại thật, đổi YOUR_LOCAL_IP thành IPv4 của máy tính.
+// Ví dụ: http://192.168.1.100:5000/api
+export const API_BASE_URL = 'http://172.16.0.180:5000/api';
---
+++ SmartTravelNguHanhSon/frontend/src/config/google.ts
@@ -0,0 +1,5 @@
+export const GOOGLE_CONFIG = {
+  webClientId: '310560169925-emv9cabuqeh2fjhgkptkmavbfgdgqbnh.apps.googleusercontent.com', // Dành cho Web / Backend verification
+  iosClientId: '310560169925-gd7rg6uokhqu75q5m274tv5ugd5nh31r.apps.googleusercontent.com', // Dành cho iOS Standalone App
+  androidClientId: 'YOUR_GOOGLE_ANDROID_CLIENT_ID', // Dành cho Android Standalone App
+};
